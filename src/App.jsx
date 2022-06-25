3b8cc8113844c6b3c3b92179b128b250e7cef78f
Aymen <76121581+aymks@users.noreply.github.com>
2022-06-25 07:33:36 +0100

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,63 @@
 //import './App.css';
 // import UilReact from '@iconscout/react-unicons/icons/uil-react'
-import TopButton from './components/TopButton';
-import Inputs from './components/Inputs';
-import TimeAndLocation from './components/TimeAndLocation';
-import TemperatueAndDetails from './components/TemperatueAndDetails';
+import TopButton from "./components/TopButton";
+import Inputs from "./components/Inputs";
+import TimeAndLocation from "./components/TimeAndLocation";
+import TemperatueAndDetails from "./components/TemperatueAndDetails";
+import Forecast from "./components/Forecast";
+import { useEffect, useState } from "react";
 /* eslint-disable */
-import Cloud from './components/Effects/Cloud/Cloud';
-import Snow from './components/Effects/Snow/Snow';
-import Cloud2 from './components/Effects/Cloud2/Cloud2';
-import Rain from './components/Effects/Rain/Rain'
-import Sunny from './components/Effects/Sunny/Sunny';
+import Cloud from "./components/Effects/Cloud/Cloud";
+import Snow from "./components/Effects/Snow/Snow";
+import Cloud2 from "./components/Effects/Cloud2/Cloud2";
+import Rain from "./components/Effects/Rain/Rain";
+import Sunny from "./components/Effects/Sunny/Sunny";
+import getFormattedWeatherData from "./services/weatherService";
 
 function App() {
+  const [query, setQuery] = useState({ q: "casablanca" });
+  const [units, setUnits] = useState("metric");
+  const [weather, setWeather] = useState(null);
+
+  useEffect(() => {
+    const fetchWeather = async () => {
+      await getFormattedWeatherData({ ...query, units }).then((data) => {
+        setWeather(data);
+      });
+    };
+
+    fetchWeather();
+  }, [query, units]);
+
   return (
-    <div className='m-0 p-0'>
-    <div className='bkg'><Rain/> </div>
-    <div className='mx-auto max-w-screen-md  py-5  px-32  h-fit shadow-xl shadow-black-400 ff'>
-      
-      <div id="color-blue">
-      
-      {/* <div id="Cloud"></div>
+    <div className="m-0 p-0">
+      <div className="bkg">
+        <Cloud />{" "}
+      </div>
+      <div className="mx-auto py-8 max-w-screen-md px-32  h-fit shadow-xl shadow-black-400 ff">
+        <div id="color-blue">
+          {/* <div id="Cloud"></div>
       <div id="Cloude"><Cloud2/></div>
         <div id="Snow"><Snow/></div>
         <Rain/> 
         <Sunny />
           <Snow/> 
         */}
-  <Cloud/>
-
+          <Cloud />
         </div>
-        
-    <TopButton />
-    <Inputs />
 
-    <TimeAndLocation />
-    <TemperatueAndDetails />
-    
-    </div>
+        <TopButton setQuery={setQuery} />
+        <Inputs setQuery={setQuery} unit setUnits={setUnits} />
+
+        {weather && (
+          <div>
+            <TimeAndLocation weather={weather} />
+            <TemperatueAndDetails weather={weather} />
+            <Forecast title="Hourly forecast" items={weather.hourly} />
+            <Forecast title="Daily forecast" items={weather.daily} />
+          </div>
+        )}
+      </div>
     </div>
   );
 }
--- src/components/TopButton.jsx
+++ src/components/TopButton.jsx
@@ -1,25 +1,25 @@
 import React from 'react'
 
-export default function TopButton() {
+function TopButton({setQuery}) {
     const cities = [
             {
                 id:1,
                 title:"London"
             },
             {
-                id:1,
+                id:2,
                 title:"Sydney"
             },
             {
-                id:1,
+                id:3,
                 title:"Tokyo"
             },
             {
-                id:1,
+                id:4,
                 title:"Toronto"
             },
             {
-                id:1,
+                id:5,
                 title:"Paris"
             },
     ]
@@ -27,8 +27,13 @@ export default function TopButton() {
     <div className='flex items-center justify-around my-6 '>
     {cities.map((city) =>(
         
-        <button key={city.id} className='text-white transition ease-out hover:scale-110'>{city.title}</button>
+        <button 
+        key={city.id} className="text-white text-lg font-medium transition ease-out hover:scale-110"
+        onClick={() => setQuery({ q: city.title})}
+        >{city.title}</button>
         ))}
     </div>
   )
 }
+
+export default TopButton
